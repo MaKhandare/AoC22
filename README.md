2c5cd2a9796df87994a334cb1db7997be6d86050
MaKhandare <khandare.matthaeus@gmail.com>
2022-11-28 21:25:23 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AoC 2022
+
+These are my solutions for the Advent of Code 2022 event.
+
+I decided to work on them in Java, simply because I am the most comfortable with it.
+
+Note: While I enjoy writing clean and well documented code, these solutions only had one goal: Getting my stars as fast as possible. Hence, some of my solutions might look like a disaster.
---
+++ day23/Main.java
@@ -0,0 +1,24 @@
+package day23;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<Integer> data = new ArrayList<>();
+
+        String fileName = "c:\\dev\\AoC22\\day1\\data.txt";
+        FileReader fileReader = new FileReader(fileName);
+
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                data.add(Integer.parseInt(line));
+            }
+        }
+
+    }
+}
