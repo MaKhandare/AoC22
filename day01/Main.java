d5bfc438c686abf10984b88a6f9146ac3695ebcf
MaKhandare <khandare.matthaeus@gmail.com>
2022-12-01 06:32:41 +0100

    d1

--- day01/Main.java
+++ day01/Main.java
@@ -1,24 +1,41 @@
 package day01;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
         ArrayList<Integer> data = new ArrayList<>();
 
-        String fileName = "c:\\dev\\AoC22\\day1\\data.txt";
+        String fileName = "c:\\dev\\AoC22\\day01\\data.txt";
         FileReader fileReader = new FileReader(fileName);
 
+        int sum = 0;
         try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                data.add(Integer.parseInt(line));
+                if (!line.isEmpty()) {
+                    sum += Integer.parseInt(line);
+                } else {
+                    data.add(sum);
+                    sum = 0;
+                }
             }
         }
+
+        Collections.sort(data);
+
+        System.out.println(data.get(data.size()-1));
+        System.out.println(data.get(data.size()-2));
+        System.out.println(data.get(data.size()-3));
+
+        sum = 0;
+        sum = data.get(data.size()-1) + data.get(data.size()-2) + data.get(data.size()-3);
+        System.out.println(sum);
 
     }
 }
