66f8b11d184f79eb52e64fba798a1eed1de44ff1
MaKhandare <khandare.matthaeus@gmail.com>
2022-12-03 08:21:24 +0100

    cleanup

--- day03/Main.java
+++ day03/Main.java
@@ -19,50 +19,49 @@ public class Main {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
 
-                // System.out.println(line);
                 data.add(line);
-                // String s1 = line.substring(0, line.length() / 2);
-                // String s2 = line.substring(line.length() / 2);
-                // sum = extracted(sum, s1, s2);
+
+                String s1 = line.substring(0, line.length() / 2);
+                String s2 = line.substring(line.length() / 2);
+                sum = part1(sum, s1, s2);
 
             }
         }
 
-        System.out.println(data);
+        sum2 = part2(data, sum2);
 
-        sum2 = extracted2(data, sum2);
-        // sum2 = extracted2(data, sum2);
-
-        // System.out.println(sum);
-        System.out.println(sum2);
+        System.out.println("Part1: " + sum);
+        System.out.println("Part2: " + sum2);
 
     }
 
-    private static int extracted2(ArrayList<String> data, int sum2) {
+    private static int part2(ArrayList<String> data, int sum2) {
         for (int i = 0; i < data.size(); i += 3) {
 
             String elfA = data.get(i);
             String elfB = data.get(i + 1);
             String elfC = data.get(i + 2);
 
-            System.out.println("A: " + elfA);
-            System.out.println("B: " + elfB);
-            System.out.println("C: " + elfC);
+            // System.out.println("A: " + elfA);
+            // System.out.println("B: " + elfB);
+            // System.out.println("C: " + elfC);
 
-            sum2 = getCom(elfA, elfB, elfC, sum2);
+            sum2 = getSum(elfA, elfB, elfC, sum2);
         }
+
         return sum2;
     }
 
-    private static int getCom(String elfA, String elfB, String elfC, int sum2) {
+    private static int getSum(String elfA, String elfB, String elfC, int sum2) {
         for (int j = 0; j < elfA.length(); j++) {
+
             for (int k = 0; k < elfB.length(); k++) {
 
                 if (elfA.charAt(j) == elfB.charAt(k)) {
 
                     for (int l = 0; l < elfC.length(); l++) {
                         if (elfB.charAt(k) == elfC.charAt(l)) {
-                            System.out.println(elfC.charAt(l));
+                            // System.out.println(elfC.charAt(l));
 
                             if (Character.isLowerCase(elfC.charAt(l))) {
                                 sum2 += (int) elfC.charAt(l) - 96;
@@ -79,15 +78,15 @@ public class Main {
             }
         }
 
-        return sum2;
+        return -1;
     }
 
-    private static int extracted(int sum, String s1, String s2) {
+    private static int part1(int sum, String s1, String s2) {
         for (int i = 0; i < s1.length(); i++) {
             for (int j = 0; j < s2.length(); j++) {
 
                 if (s1.charAt(i) == s2.charAt(j)) {
-                    System.out.println(s2.charAt(j));
+                    // System.out.println(s2.charAt(j));
 
                     if (Character.isLowerCase(s2.charAt(j))) {
                         // System.out.println((int) s2.charAt(j) - 96);
@@ -105,6 +104,6 @@ public class Main {
                 }
             }
         }
-        return sum;
+        return -1;
     }
 }
