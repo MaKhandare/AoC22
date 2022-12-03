2af05235da30e6273d45e7a48b6f4516088362b3
MaKhandare <khandare.matthaeus@gmail.com>
2022-12-03 11:46:06 +0100

    changed from char comparison to sets

--- day03/Main.java
+++ day03/Main.java
@@ -2,110 +2,97 @@ package day03;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.HashSet;
+
+import Util.InputHandler;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         // maybe also possible with sets instead of char comparisons?
+        InputHandler inputHandler = new InputHandler(".\\day03\\data.txt");
+        ArrayList<String> data = inputHandler.getData();
 
-        ArrayList<String> data = new ArrayList<>();
+        int sum = part1(data);
+        int sum2 = part2(data);
 
-        String fileName = ".\\day03\\data.txt";
-        FileReader fileReader = new FileReader(fileName);
+        System.out.println("Part 1: " + sum);
+        System.out.println("Part 2: " + sum2);
 
+    }
+
+    private static int part1(ArrayList<String> data) {
+        HashSet<Character> s1 = new HashSet<>();
+        HashSet<Character> s2 = new HashSet<>();
         int sum = 0;
-        int sum2 = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
+        for (int i = 0; i < data.size(); i++) {
+            String str1 = data.get(i).substring(0, data.get(i).length() / 2);
+            String str2 = data.get(i).substring(data.get(i).length() / 2);
 
-                data.add(line);
-
-                String s1 = line.substring(0, line.length() / 2);
-                String s2 = line.substring(line.length() / 2);
-                sum = part1(sum, s1, s2);
-
+            for (int j = 0; j < str1.length(); j++) {
+                s1.add(str1.charAt(j));
             }
+
+            for (int j = 0; j < str2.length(); j++) {
+                s2.add(str2.charAt(j));
+            }
+
+            s1.retainAll(s2);
+
+            for (Character c : s1) {
+                if (Character.isUpperCase(c)) {
+                    sum += c - 38;
+                } else {
+                    sum += c - 96;
+                }
+            }
+
+            s1.clear();
+            s2.clear();
         }
 
-        sum2 = part2(data, sum2);
-
-        System.out.println("Part1: " + sum);
-        System.out.println("Part2: " + sum2);
-
+        return sum;
     }
 
-    private static int part2(ArrayList<String> data, int sum2) {
+    private static int part2(ArrayList<String> data) {
+        HashSet<Character> s1 = new HashSet<>();
+        HashSet<Character> s2 = new HashSet<>();
+        HashSet<Character> s3 = new HashSet<>();
+        int sum = 0;
         for (int i = 0; i < data.size(); i += 3) {
+            String str1 = data.get(i);
+            String str2 = data.get(i + 1);
+            String str3 = data.get(i + 2);
 
-            String elfA = data.get(i);
-            String elfB = data.get(i + 1);
-            String elfC = data.get(i + 2);
+            for (int j = 0; j < str1.length(); j++) {
+                s1.add(str1.charAt(j));
+            }
 
-            // System.out.println("A: " + elfA);
-            // System.out.println("B: " + elfB);
-            // System.out.println("C: " + elfC);
+            for (int j = 0; j < str2.length(); j++) {
+                s2.add(str2.charAt(j));
+            }
 
-            sum2 = getSum(elfA, elfB, elfC, sum2);
-        }
+            for (int j = 0; j < str3.length(); j++) {
+                s3.add(str3.charAt(j));
+            }
 
-        return sum2;
-    }
+            s2.retainAll(s3);
+            s1.retainAll(s2);
 
-    private static int getSum(String elfA, String elfB, String elfC, int sum2) {
-        for (int j = 0; j < elfA.length(); j++) {
-
-            for (int k = 0; k < elfB.length(); k++) {
-
-                if (elfA.charAt(j) == elfB.charAt(k)) {
-
-                    for (int l = 0; l < elfC.length(); l++) {
-                        if (elfB.charAt(k) == elfC.charAt(l)) {
-                            // System.out.println(elfC.charAt(l));
-
-                            if (Character.isLowerCase(elfC.charAt(l))) {
-                                sum2 += (int) elfC.charAt(l) - 96;
-                                return sum2;
-                            }
-
-                            else {
-                                sum2 += (int) elfC.charAt(l) - 38;
-                                return sum2;
-                            }
-                        }
-                    }
+            for (Character c : s1) {
+                if (Character.isUpperCase(c)) {
+                    sum += c - 38;
+                } else {
+                    sum += c - 96;
                 }
             }
+
+            s1.clear();
+            s2.clear();
+            s3.clear();
         }
 
-        return -1;
+        return sum;
     }
 
-    private static int part1(int sum, String s1, String s2) {
-        for (int i = 0; i < s1.length(); i++) {
-            for (int j = 0; j < s2.length(); j++) {
-
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    // System.out.println(s2.charAt(j));
-
-                    if (Character.isLowerCase(s2.charAt(j))) {
-                        // System.out.println((int) s2.charAt(j) - 96);
-                        sum += (int) s2.charAt(j) - 96;
-                        return sum;
-                    }
-
-                    else {
-                        // System.out.println((int) s2.charAt(j) - 38);
-                        sum += (int) s2.charAt(j) - 38;
-                        return sum;
-
-                    }
-
-                }
-            }
-        }
-        return -1;
-    }
 }
